be359104c6746afca48287a2c50c961fcbc98e97
JUNGSANGYUN <sdsd98987@gmail.com>
2023-05-29 18:46:54 +0900

    [실습] Chpter 1.4
    1장 끝

--- w1/src/main/java/org/zerock/w1/todo/TodoReadController.java
+++ w1/src/main/java/org/zerock/w1/todo/TodoReadController.java
@@ -9,18 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(name = "todoListController",urlPatterns = "/todo/list")
-public class TodoListController extends HttpServlet {
+@WebServlet(name = "todoReadController",urlPatterns = "/todo/read")
+public class TodoReadController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("/todo/list");
+        System.out.println("/todo/read");
 
-        List<TodoDTO> dtoList = TodoService.INSTANCE.getList();
-
-        req.setAttribute("list",dtoList);
-
-        req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req,resp);
+        long tno = Long.parseLong(req.getParameter("tno"));
+        TodoDTO dto = TodoService.INSTANCE.get(tno);
+        req.setAttribute("dto", dto);
+        req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req,resp);
     }
 }
--- w1/src/main/webapp/WEB-INF/todo/read.jsp
+++ w1/src/main/webapp/WEB-INF/todo/read.jsp
@@ -2,23 +2,19 @@
   Created by IntelliJ IDEA.
   User: jeongsang-yun
   Date: 2023/05/29
-  Time: 6:02 PM
+  Time: 6:42 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-  <h1>
-    List Page
-  </h1>
-<ul>
-    <c:forEach var = "dto" items="${list}">
-        <li>${dto}</li>
-    </c:forEach>
-</ul>
+ <div>${dto.tno}</div>
+ <div>${dto.title}</div>
+ <div>${dto.dueDate}</div>
+ <div>${dto.finished}</div>
+
 </body>
 </html>
