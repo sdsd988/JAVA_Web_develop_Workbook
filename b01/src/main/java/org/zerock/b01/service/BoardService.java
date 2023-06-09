98ac2e9264c83917431b14b769c8711e2f0a4e35
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-09 21:53:50 +0900

    [실습] 게시판 CRUD

---
+++ b01/src/main/java/org/zerock/b01/service/BoardService.java
@@ -0,0 +1,14 @@
+package org.zerock.b01.service;
+
+import org.zerock.b01.dto.BoardDTO;
+
+public interface BoardService {
+
+    Long register(BoardDTO boardDTO);
+
+    BoardDTO readOne(Long bno);
+
+    void modify(BoardDTO boardDTO);
+
+    void remove(Long bno);
+}
---
+++ b01/src/test/java/org/zerock/b01/service/BoardServiceTests.java
@@ -0,0 +1,43 @@
+package org.zerock.b01.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.b01.dto.BoardDTO;
+
+@SpringBootTest
+@Log4j2
+public class BoardServiceTests {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Test
+    public void testRegister(){
+
+        log.info(boardService.getClass().getName());
+
+        BoardDTO boardDTO = BoardDTO.builder().title("Sample Title....")
+                .content("Sample Content...")
+                .writer("user00")
+                .build();
+
+        Long bno = boardService.register(boardDTO);
+
+        log.info("bno: "+ bno);
+    }
+
+    @Test
+    public void testModify(){
+        //변경에 필요한 데이터만
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(101L)
+                .title("updated....101")
+                .content("updated content 101....")
+                .build();
+
+        boardService.modify(boardDTO);
+    }
+}
