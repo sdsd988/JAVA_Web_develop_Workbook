1009d7db3a779be8cbdb86875e44d87843e5c509
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-21 00:46:29 +0900

    Validation 적용

--- b01/src/main/java/org/zerock/b01/controller/ReplyController.java
+++ b01/src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -4,12 +4,15 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b01.dto.ReplyDTO;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @RestController
@@ -19,10 +22,14 @@ public class ReplyController {
 
     @ApiOperation(value = "Replies POST",notes = "POST 방식으로 댓글 등록")
     @PostMapping(value = "/",consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String,Long>> register(@RequestBody ReplyDTO replyDTO){
+    public ResponseEntity<Map<String,Long>> register(@Valid @RequestBody ReplyDTO replyDTO,
+                                                     BindingResult bindingResult)throws BindException{
 
         log.info(replyDTO);
 
+        if (bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
         Map<String, Long> resultMap = Map.of("rno", 111L);
 
         return ResponseEntity.ok(resultMap);
