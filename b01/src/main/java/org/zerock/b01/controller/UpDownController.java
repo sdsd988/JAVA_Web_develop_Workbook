ae25f3806121859eadb61fc1eb793938c05a0820
JUNGSANGYUN <sdsd98987@gmail.com>
2023-07-05 23:07:01 +0900

    파일 조회 및 삭제

--- b01/src/main/java/org/zerock/b01/controller/UpDownController.java
+++ b01/src/main/java/org/zerock/b01/controller/UpDownController.java
@@ -5,17 +5,21 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.b01.dto.upload.UploadFileDTO;
+import org.zerock.b01.dto.upload.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @Log4j2
@@ -28,11 +32,14 @@ public class UpDownController {
     @ApiOperation(value = "Upload POST", notes = "POST 방식으로 파일 등록")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 
-    public String upload(UploadFileDTO uploadFileDTO) {
+    public List<UploadResultDTO> upload(UploadFileDTO uploadFileDTO) {
 
         log.info(uploadFileDTO);
 
         if(uploadFileDTO.getFiles() != null){
+
+            final List<UploadResultDTO> list = new ArrayList<>();
+
             uploadFileDTO.getFiles().forEach(multipartFile -> {
 
                 String originalName = multipartFile.getOriginalFilename();
@@ -42,10 +49,15 @@ public class UpDownController {
 
                 Path savePath = Paths.get(uploadPath, uuid+"_"+originalName);
 
+                boolean image = false;
+
                 try {
                     multipartFile.transferTo(savePath);
                     //이미지 파일의 종류라면
                     if(Files.probeContentType(savePath).startsWith("image")){
+
+                        image = true;
+
                         File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalName);
 
                         Thumbnailator.createThumbnail(savePath.toFile(),thumbFile,200,200);
@@ -53,8 +65,65 @@ public class UpDownController {
                 } catch (IOException e){
                     e.printStackTrace();
                 }
+
+                list.add(UploadResultDTO.builder().uuid(uuid)
+                        .fileName(originalName)
+                        .img(image).build());
             }); //end each
+            return list;
         } //end if
         return null;
     }
+
+
+    @ApiOperation(value = "view 파일", notes = "GET방식으로 첨부파일 조회")
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGet(@PathVariable String fileName){
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+        String resourceName = resource.getFilename();
+
+        HttpHeaders headers = new HttpHeaders();
+
+        try{
+            headers.add("content-Type", Files.probeContentType(resource.getFile().toPath()));
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
+
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+    @ApiOperation(value = "remove 파일", notes = "DELETE 방식으로 파일 삭제")
+    @DeleteMapping("/remove/{fileName}")
+    public Map<String, Boolean> removeFile(@PathVariable String fileName){
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+
+        String resourceName = resource.getFilename();
+
+        Map<String, Boolean> resultMap = new HashMap<>();
+        boolean removed = false;
+
+        try{
+            String contentType = Files.probeContentType(resource.getFile().toPath());
+            removed = resource.getFile().delete();
+            System.out.println(removed);
+
+            //썸네일이 존재한다면
+
+            if(contentType.startsWith("image")){
+                File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+                thumbnailFile.delete();
+
+            }
+        } catch (Exception e ){
+            log.error(e.getMessage());
+        }
+
+        resultMap.put("result", removed);
+
+        return resultMap;
+    }
+
 }
